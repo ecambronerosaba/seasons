79066ee8b57c681a1b4d837cc02bf11c46f1473c
ecambronerosaba <esteban@enya.ai>
2021-07-27 10:14:37 -0600

    Finalized seasons project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
+import SeasonDisplay from './SeasonDisplay'
+import Spinner from './Spinner';
 
 class App extends React.Component {
-    // Not required by React
-    constructor(props) {
-        // Required to call setup React.Component
-        // Subclassing it!
-        super(props);
+    // // Not required by React
+    // constructor(props) {
+    //     // Required to call setup React.Component
+    //     // Subclassing it!
+    //     super(props);
 
-        // ONLY EXCEPTION TO THE RULE 
-        // Can only do direct assignment for this
-        this.state = { lat: null, errorMessage: '' };
+    //     // ONLY EXCEPTION TO THE RULE 
+    //     // Can only do direct assignment for this
+    //     this.state = { lat: null, errorMessage: '' };
+    // }
 
+    // Can replace constructor with this totally equivalent!
+    state = { lat: null, errorMessage: '' };
+
+    componentDidMount() {
         // Don't want to call in render to refetch
         window.navigator.geolocation.getCurrentPosition(
             (position) => {
@@ -25,16 +32,24 @@ class App extends React.Component {
         );
     }
 
-    // React says we have to define render!!
-    render() {
+    renderContent() {
         if (this.state.errorMessage && !this.state.lat) {
             return <div>Error: {this.state.errorMessage} </div>;
         }
 
         if (!this.state.errorMessage && this.state.lat) {
-            return <div>Latitude: {this.state.lat}</div>;
+            return <SeasonDisplay lat={this.state.lat}/>;
         }
-        return <div>Loading . . .</div>;
+        return <Spinner text = "Please allow location access!"/>;
+    }
+
+    // React says we have to define render!!
+    render() {
+        return (
+            <div>
+                {this.renderContent()}
+            </div>
+        );
     }
 }
 
